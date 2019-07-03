4dc25584db688ab4c92b4ecdbdabc380a31ba395
Raul <Raul@10.21.9.14>
2019-07-03 11:00:43 -0500

    #cambio al estado inicial

---
+++ pruebagit1/src/pruebagit1/gitClase.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagit1;
+
+/**
+ *
+ * @author Raul
+ */
+public class gitClase {
+    
+    //un monton de acciones y cambios
+    public gitClase(){
+        String nada="";
+    }
+    
+}
